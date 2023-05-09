bbb9ed15d24183cac05bbd87bc137a583a648c05
ImaniLeemans <455181@student.fontys.nl>
2023-05-09 13:32:05 +0200

    Initial commit

---
+++ description-CUD/src/test/java/com/example/descriptioncud/DescriptionCudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.descriptioncud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescriptionCudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
