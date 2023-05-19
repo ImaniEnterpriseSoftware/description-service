a395527030aa6a166d69c2185c1f745c9b7a0274
ImaniLeemans <455181@student.fontys.nl>
2023-05-19 16:58:28 +0200

    Works on docker

--- description-CUD/src/main/java/com/example/descriptioncud/rabbitmq/RabbitConfig.java
+++ description-CUD/src/main/java/com/example/descriptioncud/rabbitmq/RabbitConfig.java
@@ -3,7 +3,6 @@ package com.example.descriptioncud.rabbitmq;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
